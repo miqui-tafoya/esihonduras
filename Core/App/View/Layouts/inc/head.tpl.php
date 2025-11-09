aec31f84b41cea9c96e1164105b76439d456f87d
ledautopica <vane.multimedia@gmail.com>
2025-11-08 20:23:15 -0600

    estilos enlaces head escritorio

--- Core/App/View/Layouts/inc/head.tpl.php
+++ Core/App/View/Layouts/inc/head.tpl.php
@@ -1,5 +1,11 @@
 <?php
 ?>
-<div id="head" class="back-head"><i class="fa-brands fa-tiktok"></i>
-<i class="fa-brands fa-youtube"></i>
-<i class="fa-brands fa-instagram"></i></div>
+<div id="head" class="back-head">
+    <div class="elementos-head">
+        <p>Nuestras redes sociales</p>
+        <a href=""> <i class="fa-brands fa-tiktok"></i></a>
+        <a href=""><i class="fa-brands fa-youtube"></i></a>
+        <a href=""> <i class="fa-brands fa-instagram"></i></a>
+    </div>
+
+</div>
