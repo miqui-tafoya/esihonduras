a39a287f2c06a7310bf63355ec33871d0e307871
miqui-tafoya <taro.miqui@gmail.com>
2025-11-10 20:53:23 -0600

    bugs visuales con menu resueltos

--- Core/App/View/Layouts/inc/menu.tpl.php
+++ Core/App/View/Layouts/inc/menu.tpl.php
@@ -15,19 +15,19 @@
     </ul>
 </div>
 <div id="menu-movil" class="slide-out back-menu-movil" style="visibility: hidden;">
-    <div style="overflow-y:auto">
-     <div class="logo-menu-movil"> <img src="Public/recursos/imagenes/logotipo-bn.png" alt=""></div>
-    <ul>
-        <li><a href="index">Inicio</a></li>
-        <li><a href="quienessomos">Quienes Somos</a></li>
-        <li><a href="saludsexual">Salud Sexual</a></li>
-        <li><a href="centrosdesalud">Centros de Salud</a></li>
-        <li><a href="informacionpublica">Información Pública</a></li>
-        <li><a href="faq">Preguntas Frecuentes</a></li>
-        <li><a href="multimedia">Multimedia</a></li>
-        <li><a href="noticias">Noticias</a></li>
-        <li><a href="contacto">Contacto</a></li>
-        <li><a id="cerrar-menu-movil" href="#">X Cerrar</a></li>
-    </ul>
+    <div class="logo-menu-movil"> <img src="Public/recursos/imagenes/logotipo-bn.png" alt=""></div>
+    <div class="menu-area">
+        <ul>
+            <li><a href="index">Inicio</a></li>
+            <li><a href="quienessomos">Quienes Somos</a></li>
+            <li><a href="saludsexual">Salud Sexual</a></li>
+            <li><a href="centrosdesalud">Centros de Salud</a></li>
+            <li><a href="informacionpublica">Información Pública</a></li>
+            <li><a href="faq">Preguntas Frecuentes</a></li>
+            <li><a href="multimedia">Multimedia</a></li>
+            <li><a href="noticias">Noticias</a></li>
+            <li><a href="contacto">Contacto</a></li>
+            <li><a id="cerrar-menu-movil" href="#">X Cerrar</a></li>
+        </ul>
     </div>
 </div>
--- Public/js/menu.js
+++ Public/js/menu.js
@@ -1,16 +1,19 @@
 const menuMovil = document.getElementById('menu-movil')
 const CerrarMenuMovil = document.getElementById('cerrar-menu-movil')
 const toggle = document.getElementById('toggle-menu')
+const body = document.body
 
 toggle.onclick = function () {
-    menuMovil.style.visibility = 'visible';
-    menuMovil.classList.add('slide-in');
-    menuMovil.classList.remove('slide-out');
+    body.style.overflow = 'hidden'
+    menuMovil.style.visibility = 'visible'
+    menuMovil.classList.add('slide-in')
+    menuMovil.classList.remove('slide-out')
 };
 CerrarMenuMovil.onclick = function () {
-    menuMovil.classList.add('slide-out');
-    menuMovil.classList.remove('slide-in');
+    body.style.overflow = 'auto'
+    menuMovil.classList.add('slide-out')
+    menuMovil.classList.remove('slide-in')
     setTimeout(() => {
-        menuMovil.style.visibility = 'hidden';
+        menuMovil.style.visibility = 'hidden'
     }, 300);
 };
