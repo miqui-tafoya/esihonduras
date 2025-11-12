352b9c74c044c87b7f1473b338e4df406c935129
ledautopica <vane.multimedia@gmail.com>
2025-11-11 22:36:49 -0600

    primeros estilos perfiles home

--- Core/App/View/Content/home.tpl.php
+++ Core/App/View/Content/home.tpl.php
@@ -1,6 +1,53 @@
 <?php
 ?>
-<div id="main">Home
-    <h1>Esi Honduras</h1>
-    <p>Plataforma de educación sexual integral</p>
+<div id="main">
+    <div class="contenedor-home">
+        <h1>Esi Honduras</h1>
+        <p>Plataforma de educación sexual integral</p>
+
+        <div class="perfiles-home">
+
+            <div class="item-perfiles-home">
+                <div class="imagen-perfil">
+                    <img src="Public/recursos/imagenes/maestra.jpg" alt="">
+                </div>
+                <div class="info-item-perfiles">
+                    <h3>Educadores</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco.</p>
+
+                </div>
+            </div>
+
+             <div class="item-perfiles-home">
+                <div class="imagen-perfil">
+                    <img src="Public/recursos/imagenes/padres.jpg" alt="">
+                </div>
+                <div class="info-item-perfiles">
+                    <h3>Madres y Padres</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco.</p>
+
+                </div>
+            </div>
+
+             <div class="item-perfiles-home">
+                <div class="imagen-perfil">
+                    <img src="Public/recursos/imagenes/jovenes.jpg" alt="">
+                </div>
+                <div class="info-item-perfiles">
+                    <h3>Jóvenes</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco.</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
