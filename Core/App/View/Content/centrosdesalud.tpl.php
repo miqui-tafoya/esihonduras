121d49ddfb58b78758a5df931e27a25978bd539b
ledautopica <vane.multimedia@gmail.com>
2025-11-12 18:58:04 -0600

    enlaces regios centros de salud

--- Core/App/View/Content/centrosdesalud.tpl.php
+++ Core/App/View/Content/centrosdesalud.tpl.php
@@ -11,11 +11,26 @@
             <div class="side-centros">
                 <div class="listado-enlaces">
                     <ul>
-                        <li><a href="regionsalud/1">Región 1</a></li>
-                        <li><a href="regionsalud/2">Región 2</a></li>
-                        <li><a href="regionsalud/3">Región 3</a></li>
-                        <li><a href="regionsalud/4">Región 4</a></li>
-                        <li><a href="regionsalud/5">Región 5</a></li>
+                        <li><a href="regionsalud/1">(01) Región Sanitaria Atlántida</a></li>
+                        <li><a href="regionsalud/2">(02) Región Sanitaria Colón</a></li>
+                        <li><a href="regionsalud/3">(03) Región Sanitaria Comayagua </a></li>
+                        <li><a href="regionsalud/4">(04) Región Sanitaria Copan</a></li>
+                        <li><a href="regionsalud/5">(05) Región Sanitaria Cortés </a></li>
+                        <li><a href="regionsalud/6">(06) Región Sanitaria Choluteca  </a></li>
+                        <li><a href="regionsalud/7">(07) Región Sanitaria El Paraíso </a></li>
+                        <li><a href="regionsalud/8">(08) Región Sanitaria Francisco Morazán</a></li>
+                        <li><a href="regionsalud/9">(09) Región Sanitaria Gracias a Dios</a></li>
+                        <li><a href="regionsalud/10">(10) Región Sanitaria Intibucá</a></li>
+                        <li><a href="regionsalud/11">(11) Región Sanitaria Islas de la Bahía</a></li>
+                        <li><a href="regionsalud/12">(12) Región Sanitaria La Paz</a></li>
+                        <li><a href="regionsalud/13">(13) Región Sanitaria Lempira </a></li>
+                        <li><a href="regionsalud/14">(14) Región Sanitaria Ocotepeque </a></li>
+                        <li><a href="regionsalud/15">(15) Región Sanitaria Olancho </a></li>
+                        <li><a href="regionsalud/16">(16) Región Sanitaria Santa Barbara</a></li>
+                        <li><a href="regionsalud/17">(17) Región Sanitaria Valle</a></li>
+                        <li><a href="regionsalud/18">(18) Región Sanitaria Yoro</a></li>
+                        <li><a href="regionsalud/19">(19) Región Metropolitana Tegucigalpa</a></li>
+                        <li><a href="regionsalud/20">(20) Región Metropolitana San Pedro Sula</a></li>
                     </ul>
                 </div>
             </div>
