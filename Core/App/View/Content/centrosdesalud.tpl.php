485b0d9d69115b36ded4959b2ab94f38fdd0e795
ledautopica <vane.multimedia@gmail.com>
2025-11-12 18:39:38 -0600

    estilos escritorio centros de salud

--- Core/App/View/Content/centrosdesalud.tpl.php
+++ Core/App/View/Content/centrosdesalud.tpl.php
@@ -2,29 +2,30 @@
     <div class="contenedor-centros">
         <div class="txt-centros">
             <p class="centros-uno">Centros de Salud</p>
-            <p class="centros-dos">Conoce el mapa interactivo Derechos ESI, en el podrás encontrar la ubicación de hospitales y centros de salud que brindan servicios de salud sexual y reproductiva en Honduras.
-</p>
+            <p class="centros-dos">Conoce el mapa interactivo Derechos ESI, en el podrás encontrar la ubicación de
+                hospitales y centros de salud que brindan servicios de salud sexual y reproductiva en Honduras.
+            </p>
         </div>
 
         <div class="grid-centros">
             <div class="side-centros">
                 <div class="listado-enlaces">
-                <ul>
-                    <li><a href="regionsalud/1">Región 1</a></li>
-                    <li><a href="regionsalud/2">Región 2</a></li>
-                    <li><a href="regionsalud/3">Región 3</a></li>
-                    <li><a href="regionsalud/4">Región 4</a></li>
-                    <li><a href="regionsalud/5">Región 5</a></li>
-                </ul>
+                    <ul>
+                        <li><a href="regionsalud/1">Región 1</a></li>
+                        <li><a href="regionsalud/2">Región 2</a></li>
+                        <li><a href="regionsalud/3">Región 3</a></li>
+                        <li><a href="regionsalud/4">Región 4</a></li>
+                        <li><a href="regionsalud/5">Región 5</a></li>
+                    </ul>
                 </div>
             </div>
 
-        </div>
-
-        <div class="contenedor-mapa">
-            <div class="mapa">
-            <img src="Public/recursos/imagenes/mapa-honduras.png" alt="">
+            <div class="contenedor-mapa">
+                <div class="mapa">
+                    <img src="Public/recursos/imagenes/mapa-honduras.png" alt="">
+                </div>
             </div>
         </div>
+
     </div>
 </div>
