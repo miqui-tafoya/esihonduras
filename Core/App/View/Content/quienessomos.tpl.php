29f22e9074935e353fd7f04ae7e009a33ec65905
ledautopica <vane.multimedia@gmail.com>
2025-11-17 13:28:41 -0600

    primer bloque quiénes somos

--- Core/App/View/Content/quienessomos.tpl.php
+++ Core/App/View/Content/quienessomos.tpl.php
@@ -1,14 +1,24 @@
 <div id="main">
 
     <div class="contenedor-quienes">
+
         <!-- inicio bloque uno -->
         <div class="bloque-uno-quienes">
-            <div class="img-blq-uno-quienes">
-
+            <div class="img-intro-quienes">
+                 <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
             </div>
             <div class="txt-intro-quienes">
-                <p>
+                <p class="uno-intro-quienes">
                     Quiénes Somos
+                </p>
+                <p class="dos-intro-quienes">
+                    La salud sexual y reproductiva es un componente esencial del bienestar humano, el desarrollo social
+                    y la equidad de género. En Honduras, garantizar el acceso a información confiable y servicios
+                    adecuados en esta área sigue siendo un desafío significativo, especialmente para poblaciones
+                    vulnerables como jóvenes, mujeres en situación de pobreza y comunidades rurales. La creación de una
+                    Plataforma de Salud Sexual y Reproductiva “Derechos ESI Honduras” representa una oportunidad
+                    innovadora para cerrar estas brechas y fortalecer el sistema de salud en el país.
+
 
                 </p>
 
