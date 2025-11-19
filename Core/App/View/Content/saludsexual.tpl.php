4940d075655dfa252cbdf4ec43ec8084a4191a86
ledautopica <vane.multimedia@gmail.com>
2025-11-18 21:17:37 -0600

    layout salud sexual

--- Core/App/View/Content/saludsexual.tpl.php
+++ Core/App/View/Content/saludsexual.tpl.php
@@ -1,9 +1,41 @@
 <div id="main">
-      <div class="contenedor-ss">
-        <div>
-            <p class="cabecera-uno">¿Qué es la salud sexual? </p>
-             <p class="cuerpo-texto">La salud sexual es el bienestar físico, mental y social en relación con la sexualidad, para lograrlo es importante que los derechos sexuales de todas las personas se respeten, protejan y se ejerzan plenamente. </p>
-        </div>
+    <div class="contenedor-ss">
 
-      </div>
+        <!-- Inicio bloque uno -->
+        <div class="bloque-fr2">
+            <div class="txt-intro-fr2">
+                <p class="cabecera-uno">¿Qué es la salud sexual?</p>
+                <p class="cuerpo-texto">
+                    La salud sexual es el bienestar físico, mental y social en relación con la sexualidad, para lograrlo
+                    es importante que los derechos sexuales de todas las personas se respeten, protejan y se ejerzan
+                    plenamente.
+                </p>
+            </div>
+
+            <div class="img-fr2">
+                <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
+            </div>
+
+        </div>
+        <!-- Fin bloque uno -->
+
+        <!-- Inicio bloque uno -->
+        <div class="bloque-fr2">
+            <div class="img-fr2">
+                <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
+            </div>
+
+            <div class="txt-intro-fr2">
+                <p class="cabecera-uno">¿Por qué hablamos de salud sexual integral?</p>
+                <p class="cuerpo-texto">
+                    La salud sexual no se logra solo con un elemento, imagina que es una receta, lograr un gran platillo
+                    requiere de varios elementos para lograr lo que se busca, lo mismo pasa con la salud sexual, es un
+                    derecho muy amplio que nos lleva a hablar de servicios de salud de buena calidad, educación,
+                    consentimiento, información, prevención.
+                </p>
+            </div>
+        </div>
+        <!-- Fin bloque uno -->
+
+    </div>
 </div>
