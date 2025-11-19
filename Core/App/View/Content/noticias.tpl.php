8e4d03944e1b264901ce43d5fa8c142c3bed08bd
ledautopica <vane.multimedia@gmail.com>
2025-11-19 14:04:13 -0600

    estilos layout grid uno noticias

--- Core/App/View/Content/noticias.tpl.php
+++ Core/App/View/Content/noticias.tpl.php
@@ -7,5 +7,25 @@
         <p class="cuerpo-texto">Aquí podrás encontrar información de tu interés</p>
     </div>
 
+    <div class="grid-uno-noticias">
+        <div>
+            <p class="cuerpo-texto">Categorias</p>
+            <ul>
+                <li><a href="">Categoria 1</a></li>
+                <li><a href="">Categoria 2</a></li>
+                <li><a href="">Categoria 3</a></li>
+                <li><a href="">Categoria 4</a></li>
+                <li><a href="">Categoria 5</a></li>
+            </ul>
+        </div>
+         <div class="img-grid-uno-noticias">
+                <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
+            </div>
+
+        <div>
+            <p class="cuerpo-texto">Entradas destacadas</p>
+        </div>    
+    </div>
+
     </div>
 </div>
