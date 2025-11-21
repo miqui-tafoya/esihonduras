c59a62e7318b501077ec99aedfd07cbdfc9ce394
ledautopica <vane.multimedia@gmail.com>
2025-11-21 12:26:36 -0600

    estilos categorias blog escritorio

--- Core/App/View/Content/noticias.tpl.php
+++ Core/App/View/Content/noticias.tpl.php
@@ -2,30 +2,37 @@
 
     <div class="contenedor-noticias">
 
-    <div class="intro-noticias">
-        <p class="cabecera-uno">Noticias</p>
-        <p class="cuerpo-texto">Aquí podrás encontrar información de tu interés</p>
-    </div>
-
-    <div class="grid-uno-noticias">
-        <div>
-            <p class="cuerpo-texto">Categorias</p>
-            <ul>
-                <li><a href="">Categoria 1</a></li>
-                <li><a href="">Categoria 2</a></li>
-                <li><a href="">Categoria 3</a></li>
-                <li><a href="">Categoria 4</a></li>
-                <li><a href="">Categoria 5</a></li>
-            </ul>
+        <div class="intro-noticias">
+            <p class="cabecera-uno">Noticias</p>
+            <p class="cuerpo-texto">Aquí podrás encontrar información de tu interés</p>
         </div>
-         <div class="img-grid-uno-noticias">
-                <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
+
+        <!-- Grid uno -->
+        <div class="grid-uno-noticias">
+
+            <div class="list-categorias">
+                <p class="cabecera-blog-uno">Categorias</p>
+                <ul class="bloque-list-categorias">
+                    <li><a href="">Categoria 1</a></li>
+                    <li><a href="">Categoria 2</a></li>
+                    <li><a href="">Categoria 3</a></li>
+                    <li><a href="">Categoria 4</a></li>
+                    <li><a href="">Categoria 5</a></li>
+                </ul>
             </div>
 
-        <div>
-            <p class="cuerpo-texto">Entradas destacadas</p>
-        </div>    
-    </div>
+            <div class="bloque-foto-noticias">
+                <div class="img-grid-noticias">
+                    <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
+                </div>
+            </div>
+
+            <div class="bloque-destacadas">
+                <p class="cabecera-blog-uno">Entradas destacadas</p>
+            </div>
+
+        </div>
+        <!-- Fin uno -->
 
     </div>
 </div>
