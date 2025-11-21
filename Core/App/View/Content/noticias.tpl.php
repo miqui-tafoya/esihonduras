f05175fb77606dc0ca1cdf193daed6f25e398a72
ledautopica <vane.multimedia@gmail.com>
2025-11-21 13:15:55 -0600

    estilos link ffoto blog

--- Core/App/View/Content/noticias.tpl.php
+++ Core/App/View/Content/noticias.tpl.php
@@ -23,7 +23,19 @@
 
             <div class="bloque-foto-noticias">
                 <div class="img-grid-noticias">
-                    <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
+
+                    <div class="link-destacada">
+                        <a href="">
+                            <p class="txt-destacada-uno">Titulo artículo</p>
+                            <p class="txt-destacada-dos">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                                aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                pariatur. </p>
+                        </a>
+                    </div>
+
+                    <img src="Public/recursos/imagenes/foto_3.jpg" alt="">
                 </div>
             </div>
 
