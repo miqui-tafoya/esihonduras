134f9fb832055df34e75f52743888fab03c78ba1
ledautopica <vane.multimedia@gmail.com>
2025-11-21 21:51:35 -0600

    foto grid uno noticias movil

--- Core/App/View/Content/noticias.tpl.php
+++ Core/App/View/Content/noticias.tpl.php
@@ -29,9 +29,7 @@
                         <p class="txt-destacada-uno">Titulo artículo</p>
                         <p class="txt-destacada-dos">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. </p>
+                            quis nostrud exercitation ullamco laboris. </p>
                         <div class="link-info-destacado">
                             <a href="">Leer más</a>
                         </div>
