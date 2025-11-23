8c74b05cd8ca1be419f7baad9c63766d6f56825c
ledautopica <vane.multimedia@gmail.com>
2025-11-23 13:23:13 -0600

    bloque seis escritorio salud sexual

--- Core/App/View/Content/saludsexual.tpl.php
+++ Core/App/View/Content/saludsexual.tpl.php
@@ -5,12 +5,10 @@
         <div class="bloque-uno-ss">
             <div class="bloque-fr2">
                 <div class="txt-intro-fr2">
-                    <p class="cabecera-uno-ss">¿Qué es la salud sexual?</p>
+                    <p class="cabecera-uno-ss">¿Por qué hablamos de salud sexual integral?</p>
                     <p class="cuerpo-texto-uno-ss">
-                        La salud sexual es el bienestar físico, mental y social en relación con la sexualidad, para
-                        lograrlo
-                        es importante que los derechos sexuales de todas las personas se respeten, protejan y se ejerzan
-                        plenamente.
+                        Hablar de salud sexual es hablar del derecho a la salud de todas las personas y los derechos no
+                        se le niega a nadie.
                     </p>
                 </div>
 
@@ -24,23 +22,22 @@
 
         <!-- Inicio bloque uno -->
         <div class="bloque-dos-ss">
-            <div class="bloque-fr2">
-                <div class="img-fr2">
-                    <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
-                </div>
+            <div class="info-bloque-tres">
+                <p class="cabecera-uno">¿Qué es la salud sexual? </p>
+                <p class="cuerpo-texto">La salud sexual es el bienestar físico, mental y social en relación con la
+                    sexualidad, para lograrlo es importante que los derechos sexuales de todas las personas se respeten,
+                    protejan y se ejerzan plenamente.</p>
+                <br>
+                <br>
+                <p class="cabecera-uno">¿Por qué hablamos de salud sexual integral? </p>
+                <p class="cuerpo-texto">La salud sexual no se logra solo con un elemento, imagina que es una receta,
+                    lograr un gran platillo requiere de varios elementos para lograr lo que se busca, lo mismo pasa con
+                    la salud sexual, es un derecho muy amplio que nos lleva a hablar de servicios de salud de buena
+                    calidad, educación, consentimiento, información, prevención. </p>
 
-                <div class="txt-intro-fr2">
-                    <p class="cabecera-uno">¿Por qué hablamos de salud sexual integral?</p>
-                    <p class="cuerpo-texto">
-                        La salud sexual no se logra solo con un elemento, imagina que es una receta, lograr un gran
-                        platillo
-                        requiere de varios elementos para lograr lo que se busca, lo mismo pasa con la salud sexual, es
-                        un
-                        derecho muy amplio que nos lleva a hablar de servicios de salud de buena calidad, educación,
-                        consentimiento, información, prevención.
-                    </p>
-                </div>
             </div>
+
+
         </div>
         <!-- Fin bloque uno -->
 
@@ -106,23 +103,124 @@
 
         <!-- Inicio bloque cinco -->
         <div class="bloque-cinco-ss">
-            <div class="info-bloque-tres">
-                <p class="cabecera-uno">¿Para quién son los derechos sexuales?</p>
-                <p class="cuerpo-texto">¡Para todas las personas!, sin excluir a nadie. <br>
-                    Los derechos sexuales y reproductivos se basan en la autonomía de las personas para tomar decisiones
-                    sobre su vida, cuerpo, sexualidad, reproducción, salud y bienestar y toda esta información es
-                    importante para todas las personas sin importar la edad. <br> <br>
-                    ¿Todas las personas? <br>
-                    Sí, a las niñas y niños se les enseña sobre higiene, que nadie absolutamente nadie puede tocar su
-                    cuerpo, en la adolescencia de la menstruación, del deporte y alimentación sana, en la juventud de
-                    las relaciones sanas y sin violencia en el noviazgo, de enfermedades infecciosas, de sexualidad con
-                    información actualizada, veraz, completa, científica, al decidir ser madres que recibían atención
-                    médica y que nadie puede violentarse.
-                </p>
+
+            <div class="bloque-fr2">
+                <div class="img-fr2">
+                    <img src="Public/recursos/imagenes/foto_3.jpg" alt=""></a>
+                </div>
+
+                <div class="txt-intro-fr2">
+                    <p class="cabecera-uno">¿Para quién son los derechos sexuales?</p>
+                    <p class="cuerpo-texto">
+                        ¡Para todas las personas!, sin excluir a nadie. <br>
+                        Los derechos sexuales y reproductivos se basan en la autonomía de las personas para tomar
+                        decisiones sobre su vida, cuerpo, sexualidad, reproducción, salud y bienestar y toda esta
+                        información es importante para todas las personas sin importar la edad.<br><br>
+                        ¿Todas las personas?<br><br>
+                        Sí, a las niñas y niños se les enseña sobre higiene, que nadie absolutamente nadie puede tocar
+                        su cuerpo, en la adolescencia de la menstruación, del deporte y alimentación sana, en la
+                        juventud de las relaciones sanas y sin violencia en el noviazgo, de enfermedades infecciosas, de
+                        sexualidad con información actualizada, veraz, completa, científica, al decidir ser madres que
+                        recibían atención médica y que nadie puede violentarse.
+
+                    </p>
+                </div>
             </div>
 
         </div>
         <!-- Fin bloque cinco -->
+
+        <!-- Inicio bloque seis-->
+        <div class="bloque-seis-ss">
+
+            <div class="info-seis-uno">
+                <p class="cabecera-seis-uno">¿Y que incluyen los derechos sexuales y reproductivos?
+                </p>
+                <p class="cuerpo-seis-uno">Incluyen el derecho a tener relaciones sexuales, sin violencia ni bajo
+                    presión; a tener hijos cuando lo desea y a expresar la sexualidad. Los Derechos Sexuales y
+                    Reproductivos tienen el respaldo de las Directrices Internacionales sobre VIH/SIDA y los Derechos
+                    Humanos, adoptadas por primera vez en 1996 y revisadas en 2002; en estas se da respuesta a la
+                    petición de la Comisión de Derechos Humanos de las Naciones Unidas, respecto a la toma de medidas
+                    concretas para proteger los derechos humanos en el contexto del VIH. (ONUSIDA. Ginebra, 2006)
+                    <br>
+                </p>
+            </div>
+
+            <div class="info-seis-dos">
+                <p class="cabecera-seis-dos"> Imagina que los derechos sexuales son como una receta,<br> para lograrlo
+                    con plenitud es importante
+                    incorporar: </p>
+                <div class="items-datos">
+                    <div class="grid-items">
+                        <div class="item">
+                            <p>Información para decidir de forma libre, autónoma sobre tu cuerpo y sexualidad.
+                            </p>
+
+                        </div>
+
+                        <div class="item">
+                            <p>Información para decidir de forma libre, autónoma sobre tu cuerpo y sexualidad.
+                            </p>
+                        </div>
+
+                        <div class="item">
+                            <p>Poder ejercer y disfrutar plenamente tu vida sexual.
+                            </p>
+                        </div>
+
+                        <div class="item">
+                            <p>Decidir con quien o quienes quieres relacionarte afectivamente, erótica y sexualmente.
+                            </p>
+                        </div>
+
+                        <div class="item">
+                            <p>Que se respete tu privacidad y que se resguarde tu información personal.
+
+                            </p>
+                        </div>
+
+                        <div class="item">
+                            <p>A decidir de manera libre e informada cuándo quiero o no ser madre o padre.
+                            </p>
+                        </div>
+
+                        <div class="item">
+                            <p>A vivir libre de discriminación, que se garantice la igualdad, la identidad sexual, la
+                                vida, la integridad física, psicológica y sexual de todas las personas.
+                            </p>
+                        </div>
+
+                        <div class="item">
+                            <p>Recibir educación integral en sexualidad actualizada, veraz, completa, científica.
+                            </p>
+                        </div>
+
+                        <div class="item">
+                            <p>A que se garanticen los servicios gratuitos e integrales de salud por el estado.
+                            </p>
+                        </div>
+
+                        <div class="item">
+                            <p>Participar y opinar en las políticas públicas sobre sexualidad y reproducción.
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+                <p class="cuerpo-seis-uno">Los derechos reproductivos son esenciales para ejercer el derecho a la salud
+                    e incluye el derecho a utilizar servicios de salud reproductiva integrales y de buena calidad, que
+                    garanticen la privacidad, el consentimiento libre e informado, la confidencialidad y sobre todo el
+                    respeto.
+                    <br>
+                </p>
+                <p class="cabecera-seis-dos"> La salud sexual significa poder disfrutar de una vida sexual sin riesgos.
+                    incorporar: </p>
+
+            </div>
+        </div>
+
+        <!-- Fin bloque seis -->
 
     </div>
 </div>
