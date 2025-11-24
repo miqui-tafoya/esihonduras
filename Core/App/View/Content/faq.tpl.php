108dd34e535fa4dce6a63daaa33eeddde606440c
ledautopica <vane.multimedia@gmail.com>
2025-11-23 20:30:46 -0600

    layout inicial faq

--- Core/App/View/Content/faq.tpl.php
+++ Core/App/View/Content/faq.tpl.php
@@ -1,8 +1,80 @@
 <div id="main">
     <div class="contenedor-faq">
-        <div class="intro-faq">
-            <p class="cabecera-uno">Preguntas frecuentes</p>
 
+        <!-- Inicio bloque intro -->
+        <div class="bloque-intro">
+            <div class="intro-faq">
+                <p class="cabecera-dos">Sabemos que puedes tener dudas, aquí te compartimos algunas respuestas</p>
+                <div>
+                    <div class="img-bloque-dos-home">
+                        <img src="Public/recursos/imagenes/fondo.png" alt="">
+                    </div>
+                </div>
+
+            </div>
         </div>
+
+        <!-- Inicio bloque intro -->
+
+        <!-- Inicio bloque Madres y Padres -->
+        <div class="bloque-intro">
+            <div class="intro-faq">
+                <p class="cabecera-dos">Preguntas más frecuentes que madres y padres se hacen sobre sexualidad de sus
+                    hijas e hijos adolescentes.
+                </p>
+
+
+            </div>
+        </div>
+
+        <!-- Inicio Madres y Padres -->
+
+        <!-- Inicio bloque Jóvenes -->
+        <div class="bloque-intro">
+            <div class="intro-faq">
+                <p class="cabecera-dos">Dudas y preguntas que puedes tener en la juventud.
+                </p>
+
+                <p class="cuerpo-texto">Seguramente tienes muchas dudas y está bien tenerlas, aquí te compartimos
+                    algunas preguntas y respuestas que están inspiradas en 100 preguntas sobre sexualidad adolescente.
+                    Consulta más preguntas y respuestas en
+                </p>
+                <div class="btn-enlace">
+                    <a href="https://www.etsex.cl/wp-content/uploads/2021/05/Chile.-Municipalidad-de-Santiago.-2016.-100-Preguntas-Sobre-Sexualidad-Adolescente.pdf"
+                        target="_blank"> 100 Preguntas sobre sxualidad adolescente</a>
+                </div>
+
+
+            </div>
+        </div>
+
+        <!-- Fin Jóvenes -->
+
+        <!-- Inicio bloque Adultos-->
+        <div class="bloque-intro">
+            <div class="intro-faq">
+                <p class="cabecera-dos">Dudas y preguntas que las personas adultas tenemos sobre la educación sexual
+                    integral.
+                </p>
+
+                <p class="cuerpo-texto">Seguramente cuando escuchas “Educación sexual integral” te surgen más dudas que
+                    respuestas. Te compartimos algunas preguntas con sus respuestas y si quieres leer más sobre el tema
+                    te invitamos a consultar:<br>
+
+                    Educación sexual integral, Organización Mundial de la Salud (OMS)
+
+                </p>
+                <div class="btn-enlace">
+                    <a href="https://www.who.int/es/news-room/questions-and-answers/item/comprehensive-sexuality-education"
+                        target="_blank"> Educación sexual integral</a>
+                </div>
+
+
+            </div>
+        </div>
+
+        <!-- Inicio Adultos -->
+
+
     </div>
 </div>
