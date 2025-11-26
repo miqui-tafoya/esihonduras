93907843aff5329544211792bc1ae016e79e431d
ledautopica <vane.multimedia@gmail.com>
2025-11-26 17:39:16 -0600

    layout contacto

--- Core/App/View/Content/contacto.tpl.php
+++ Core/App/View/Content/contacto.tpl.php
@@ -2,14 +2,54 @@
 
 <div id="main">
 
-<div class="contenedor-contacto">
-    <div class="cabecera-contacto">
-        <p class="cabecera-uno">
-            Contacto
-        </p>
+    <div class="contenedor-contacto">
+        <div class="cabecera-contacto">
+            <p class="cabecera-uno">
+                Contacto
+            </p>
+            <p class="cuerpo-texto">¿Te interesa tener más información sobre tu derecho a la salud en Honduras?</p>
+
+        </div>
+
+        <div class="bloque-info-contacto">
+
+            <!-- Presidencia -->
+
+            <div class="bloque-organizacion">
+
+                <p class="cabecera-dos-contacto">Presidencia de Honduras</p>
+                <br>
+                <div class="bloque-enlace">
+                    <p class="cuerpo-texto">Sitio oficial</p>
+                    <div class="btn-enlace"> <a href="https://www.presidencia.gob.hn/">Visitar</a></div>
+                </div>
+                <div class="bloque-enlace">
+                    <p class="cuerpo-texto">Cuenta oficial de Facebook</p>
+                    <div class="btn-enlace"> <a href="https://www.facebook.com/gobiernohn/">Visitar</a></div>
+                </div>
+                <div class="bloque-enlace">
+                    <p class="cuerpo-texto">Cuenta oficial de Instagram</p>
+                    <div class="btn-enlace"> <a href="https://www.instagram.com/gobiernohonduras/">Visitar</a></div>
+                </div>
+            </div>
+            <!--End Presidencia -->
+
+            <!-- Salud -->
+
+            <div class="bloque-organizacion">
+                <p class="cabecera-dos-contacto">Secretaria de Salud de Honduras</p>
+                <br>
+                <div class="bloque-enlace">
+                    <p class="cuerpo-texto">Cuenta oficial de Instagram</p>
+                    <div class="btn-enlace"> <a href="https://sshome.salud.gob.hn/">Visitar</a></div>
+                </div>
+            </div>
+            <!-- End Salud -->
+
+
+
+        </div>
 
     </div>
-
-</div>
 
 </div>
