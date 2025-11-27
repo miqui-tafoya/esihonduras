122595686992d7bd8cb37a24ffbee414a0ba9de3
ledautopica <vane.multimedia@gmail.com>
2025-11-27 12:49:26 -0600

    layout filtrosinfo pública

--- Core/App/View/Content/informacionpublica.tpl.php
+++ Core/App/View/Content/informacionpublica.tpl.php
@@ -7,7 +7,7 @@
                 Información Pública
             </p>
             <p class="cabecera-dos-publica">
-                La información es para todas y todos y los derechos ESI también. 
+                La información es para todas y todos y los derechos ESI también.
             </p>
             <p class="cuerpo-texto">Derechos ESI pone a tu disposición la información pública y los datos abiertos sobre
                 salud sexual y reproductiva en Honduras.
@@ -19,5 +19,44 @@
             </p>
 
         </div>
+        <div class="bloque-buscar">
+
+            <p class="cabecera-dos-publica">Buscar por palabra </p>
+            <form action="">
+                <label for="fname">Ingresa una palabra</label>
+                <input type="text" id="" name="fname"><br><br>
+
+            </form>
+
+
+        </div>
+
+        <div class="bloque-filtrar">
+            <p class="cabecera-dos-publica">Filtrar por categoría: </p>
+            <form action="#">
+                <input type="checkbox" id="protocolo" name="protocolo" value="Protocolo">
+                <label for="protocolo"> Protocolo </label><br>
+
+                <input type="checkbox" id="politica" name="politica" value="Política">
+                <label for="politica"> Política </label><br>
+
+                <input type="checkbox" id="norma" name="norma" value="Norma">
+                <label for="norma">  Norma</label><br><br>
+
+                <input type="checkbox" id="lineamientos" name="lineamientos" value="Lineamientos">
+                <label for="lineamientos">  Lineamientos</label><br><br>
+
+                <input type="checkbox" id="guias" name="guias" value="lineamientos">
+                <label for="guias">  Guías</label><br><br>
+
+                 <input type="checkbox" id="otros" name="otros" value="Otros">
+                <label for="otros">Otros</label><br><br>
+                
+              
+                <input type="submit" value="Filtrar">
+            </form>
+
+        </div>
+
     </div>
 </div>
