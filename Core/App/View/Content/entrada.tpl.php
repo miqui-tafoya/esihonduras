3e068ea492db2c062d14f27db414c704faa348c0
ledautopica <vane.multimedia@gmail.com>
2025-11-27 21:00:35 -0600

    estilos entrada responsive

--- Core/App/View/Content/entrada.tpl.php
+++ Core/App/View/Content/entrada.tpl.php
@@ -6,17 +6,25 @@ $entrada = $body['entrada'];
 
 
     <div class="contenedor-publica">
-        <div class="btn-back">
-            <?php echo ($entrada['tipo'] === 'publica') ? '<a href="' . URL_BASE . 'informacionpublica" ><i class="fa-solid fa-arrow-left-long"></i>Regresar a información pública</a>' : '<a href="' . URL_BASE . 'noticias"><i class="fa-solid fa-arrow-left-long"></i>Regresar a noticias</a>'; ?>
+        <div class="cabecera-entrada">
+            <div class="btn-back">
+                <?php echo ($entrada['tipo'] === 'publica') ? '<a href="' . URL_BASE . 'informacionpublica" ><i class="fa-solid fa-arrow-left-long"></i>Regresar a información pública</a>' : '<a href="' . URL_BASE . 'noticias"><i class="fa-solid fa-arrow-left-long"></i>Regresar a noticias</a>'; ?>
+            </div>
         </div>
 
-        <?php echo $entrada['entradas_timestamp']; ?>
-        <br>
-        <?php echo URL_PUBLIC . 'recursos/entradas/' . $entrada['portada']; ?>
-        <br>
-        <?php echo $entrada['entradas_titulo']; ?>
-        <br>
-        <?php echo $entrada['cuerpo']; ?>
+        <div class="bloque-entrada">
+             <div class="nombre-publicado"> <p><?php echo $entrada['entradas_timestamp']; ?></p></div>
+           
+           <div class="img-entrada">
+           <img src="<?php echo URL_PUBLIC . 'recursos/entradas/' . $entrada['portada']; ?>" alt=""> 
+           </div>
+          
+            <div><p class="cabecera-dos-publica"><?php echo $entrada['entradas_titulo']; ?></p> </div>
+         
+            <div> <p class="cuerpo-texto"><?php echo $entrada['cuerpo']; ?></p></div>
+        </div>
+
+
 
     </div>
 </div>
