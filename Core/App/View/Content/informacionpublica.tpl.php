d150428b5e7a83bf88d4abf38b7b068c332136fc
ledautopica <vane.multimedia@gmail.com>
2025-11-27 18:37:19 -0600

    layout publicaciones

--- Core/App/View/Content/informacionpublica.tpl.php
+++ Core/App/View/Content/informacionpublica.tpl.php
@@ -19,18 +19,21 @@
             </p>
 
         </div>
+
+        <!-- Start bloque buscar -->
         <div class="bloque-buscar">
 
             <p class="cabecera-dos-publica">Buscar por palabra </p>
-            <form action="">
-                <label for="fname">Ingresa una palabra</label>
+            <form action="" class="layout-busqueda">
+                <label class="p-bottom">Ingresa una palabra</label>
                 <input type="text" id="" name="fname"><br><br>
 
             </form>
 
-
         </div>
+        <!-- End bloque buscar -->
 
+        <!-- Start bloque filtros -->
         <div class="bloque-filtrar">
             <p class="cabecera-dos-publica">Filtrar por categoría: </p>
 
@@ -52,7 +55,8 @@
                 </div>
 
                 <div class="item-filtro">
-                    <input type="checkbox" id="lineamientos" name="lineamientos" value="Lineamientos" class="filtro-check">
+                    <input type="checkbox" id="lineamientos" name="lineamientos" value="Lineamientos"
+                        class="filtro-check">
                     <label for="lineamientos"> Lineamientos</label>
                 </div>
 
@@ -66,12 +70,44 @@
                     <label for="otros">Otros</label>
                 </div>
                 <div class="btn-enlace">
-                     <button type="submit" value="Filtrar">Filtrar<i class="fa-solid fa-filter"></i></button>
-                   
+                    <button type="submit" value="Filtrar">Filtrar<i class="fa-solid fa-filter"></i></button>
+
                 </div>
             </form>
 
         </div>
+
+        <!-- End bloque filtros -->
+
+        <!-- Start bloque información -->
+
+        <div class="bloque-items">
+
+            <p class="cabecera-dos-publica">Todas las publicaciones</p>
+
+            <div class="publicaciones">
+                <div class="item-perfiles-quienes">
+                    <div class="img-item-quienes">
+                        <img src="Public/recursos/imagenes/maestra.jpg" alt="">
+                    </div>
+                    <div class="info-item-quienes">
+                        <p class="titulo-item-quienes">Título de la publicación</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco.</p>
+                        <div class="btn-perfiles">
+                            <a href="educadores">Ir a contenido <i class="fa-solid fa-arrow-right-long"></i></a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <!-- End bloque información -->
 
     </div>
 </div>
