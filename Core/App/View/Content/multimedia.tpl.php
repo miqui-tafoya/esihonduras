4f3880d5ed76b61e22409386ae412812393d5a4b
ledautopica <vane.multimedia@gmail.com>
2025-12-02 21:40:56 -0600

    layout multimedia

--- Core/App/View/Content/multimedia.tpl.php
+++ Core/App/View/Content/multimedia.tpl.php
@@ -7,6 +7,25 @@ $publicaciones = $body['publicaciones'];
 <div id="main">
     <div class="contenedor-multimedia">
 
+        <!-- inicio bloque uno -->
+        <div class="bloque-uno-quienes">
+            <div class="img-intro-quienes">
+                <img src="Public/recursos/imagenes/quienes.png" alt=""></a>
+            </div>
+            <div class="txt-intro-quienes">
+                <p class="cabecera-uno">
+                    Multimedia
+                </p>
+                <p class="dos-intro-quienes">
+                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                </p>
+
+            </div>
+
+        </div>
+        <!-- fin bloque uno -->
+
         <!-- Start video -->
         <div class="bloque-video">
 
@@ -28,8 +47,16 @@ $publicaciones = $body['publicaciones'];
 
         <!-- End video -->
 
+        <!-- Start publicaciones -->
+        <div class="bloque-publicaciones">
+            <p>
+                Aquí van las públicaciones
+            </p>
+        </div>
+        <!-- End publicaciones -->
+
         <!-- Start audio -->
-        <div>
+        <div class="bloque-audio">
             <?php foreach ($audio as $value) {
                 echo $value['podcast_url'];
             } ?>
