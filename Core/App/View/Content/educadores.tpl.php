03d1c749adf41915f6be92de8deb3aa9579f5baa
ledautopica <vane.multimedia@gmail.com>
2025-12-04 17:06:58 -0600

    estilos bloque uno educadores

--- Core/App/View/Content/educadores.tpl.php
+++ Core/App/View/Content/educadores.tpl.php
@@ -2,46 +2,51 @@
 <div id="main">
     <div class="contenedor-educadores">
 
-        <div class="intro">
-            <p class="cabecera-uno">Educadores</p>
-        </div>
-
-        <!-- Start Bloque uno -->
-        <div class="grid-doble">
-
-            <div class="img-grid-doble">
-                <img src="Public/recursos/imagenes/embarazo_adolescente.png" alt=""></a>
+        <div class="back-color">
+            <div class="intro">
+                <p class="cabecera-uno">Educadores</p>
             </div>
-            <div class="txt-grid-doble">
 
-                <p class="cabecera-one">Información dirigida a educadores</p>
-                <p class="dos-grid-doble">Las y los maestros son parte fundamental en la formación de las chicas y
-                    chicos. Educar no solo es leer
-                    información, su rol es transmitir conocimientos que abonen a la empatía, el respeto a todas las
-                    personas, a
-                    desarrollarse en sociedades sin violencia, creando espacios seguros y de confianza donde todas las
-                    dudas
-                    sobre la sexualidad integral puedan ser escuchadas y que la información se comparta sin estigmas.
-                    <br>
-                    <br>
-                    El rol de las y los docentes: Transmitir información, escuchar sus dudas,
-                    acompañar sus inquietudes,
-                    informar sin estigmatizar.
-                    <br>
-                    <br>
-                    El Papel del Docente en la Educación para la Sexualidad: Algunas Reflexiones
-                    en el Proceso Educativo Escolar
-                    consulta en:
-                </p>
-                <div class="btn-enlace">
-                    <a href="https://www.redalyc.org/journal/447/44741347020/html/" target="_blank">Leer más</a>
+            <!-- Start Bloque uno -->
+            <div class="grid-doble">
+
+                <div class="img-grid-doble">
+                    <img src="Public/recursos/imagenes/embarazo_adolescente.png" alt=""></a>
                 </div>
+                <div class="txt-grid-doble">
 
+                    <p class="cabecera-one">Información dirigida a educadores</p>
+                    <p class="dos-grid-doble">Las y los maestros son parte fundamental en la formación de las chicas y
+                        chicos. Educar no solo es leer
+                        información, su rol es transmitir conocimientos que abonen a la empatía, el respeto a todas las
+                        personas, a
+                        desarrollarse en sociedades sin violencia, creando espacios seguros y de confianza donde todas
+                        las
+                        dudas
+                        sobre la sexualidad integral puedan ser escuchadas y que la información se comparta sin
+                        estigmas.
+                        <br>
+                        <br>
+                        El rol de las y los docentes: Transmitir información, escuchar sus dudas,
+                        acompañar sus inquietudes,
+                        informar sin estigmatizar.
+                        <br>
+                        <br>
+                        El Papel del Docente en la Educación para la Sexualidad: Algunas Reflexiones
+                        en el Proceso Educativo Escolar
+                        consulta en:
+                    </p>
+                    <div class="btn-enlace">
+                        <a href="https://www.redalyc.org/journal/447/44741347020/html/" target="_blank">Leer más</a>
+                    </div>
+
+                </div>
             </div>
+
+            <!-- End Bloque uno -->
         </div>
 
-        <!-- End Bloque uno -->
-
+        <!-- Start Bloque dos -->
         <div id="esi-presentation">
             <div id="esi-slide">
                 <div class="esi-slide-content">
@@ -53,12 +58,16 @@
                 <span id="esi-page-indicator">Página 1 / 13</span>
             </div>
         </div>
-        <div>
-            <h2>Material de consulta</h2>
+          <!-- Start Bloque dos -->
+
+           <!-- Start Bloque tres --> 
+        <div class="back-color">
+            <p class="cabecera-one">Material de consulta</p>
             <p>
 
             </p>
         </div>
+        <!-- End Bloque tres --> 
 
     </div>
 </div>
