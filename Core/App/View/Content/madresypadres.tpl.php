b543ede552b0fbf5b53788b4580229347789c977
ledautopica <vane.multimedia@gmail.com>
2025-12-07 17:39:06 -0600

    layout intro madres y padres

--- Core/App/View/Content/madresypadres.tpl.php
+++ Core/App/View/Content/madresypadres.tpl.php
@@ -1,11 +1,99 @@
 <span id="breadcrumbs">quienessomos</span>
+
 <div id="main">
+
     <div class="contenedor-madresypadres">
-        <h1>Información para Madres y Padres</h1>
+
+        <!-- Start Bloque uno -->
+
+        <div class="back-color">
+            <div class="intro">
+                <p class="cabecera-uno">Madres y Padres</p>
+            </div>
+
+
+            <div class="grid-doble">
+
+                <div class="img-grid-doble">
+                    <img src="Public/recursos/imagenes/padres.jpg" alt=""></a>
+                </div>
+                <div class="txt-grid-doble">
+
+                    <p class="cabecera-one">Información dirigida tutores </p>
+                    <p class="dos-grid-doble">Hablar de derechos sexuales y reproductivos de manera abierta, respetuosa
+                        es muy importante para brindarle a tus hijas e hijos toda la información que necesitan para
+                        tomar decisiones informadas y responsables.
+                        <br>
+                        <br>
+                        Puede que tu como madre y padre tengas dudas, lo sabemos, tener dudas es parte de acompañar, es
+                        válido preguntar y buscar información.
+                        <br>
+                        <br>
+
+                    </p>
+
+
+                </div>
+            </div>
+
+
+
+        </div>
+        <!-- End Bloque uno -->
+
+        <!-- Start Bloque dos -->
+        <div class="back-color-dos">
+            <div class="intro">
+                <p class="cabecera-dos-faq">Introducción</p>
+            </div>
+            <div class="bloque-slide">
+                <div id="esi-presentation">
+                    <p class="cuerpo-texto">La educación sexual y la educación para la salud reproductiva son elementos
+                        que se han hecho
+                        indispensables en la formación de los seres humanos.
+                        Por razones religiosas, políticas, económicas y socioculturales la información sobre sexualidad
+                        se ha ocultado y se ha tergiversado, se ha cubierto de mitos.
+                        Para prevenir situaciones tales como enfermedades de transmisión sexual no tratadas, embarazos
+                        no deseados, el haber sido víctima de abuso sexual, es necesario cambiar las creencias erróneas,
+                        y dar información sobre todos estos tema. Además, la educación sexual integral favorece las
+                        relaciones saludables, previene la violencia de género y propicia la inclusión de las
+                        diversidades.<br><br>
+                        Como madres y padres es importante estar informados para poder guiar y acompañar a las hijas e
+                        hijos, es importante tener en cuenta que muchas veces madres y padres necesitan apoyo, y
+                        entender el proceso por el que están atravesando sus hijas e hijos.
+                        <br><br>
+                        Es necesario que mamás y papás, las y los jóvenes aprendan a comunicar sus sentimientos y a
+                        expresarlos de manera honesta, así como buscar información clara y actualizada, resolver sus
+                        propias dudas y cuestionar sus propias creencias acerca de la sexualidad.
+                        <br><br>
+                        A continuación se sugieren algunos temas para abordar en la educación sexual integral de las
+                        adolescencias. <br><br>
+
+                    </p>
+                    <p class="cabecera-one-dos">En esta secciones encontrarás información sobre: </p>
+                    <p class="cuerpo-texto"><span>&#8226;</span>Aspectos biológicos de la sexualidad<br>
+                        <span>&#8226;</span>Alternativas para ejercer la sexualidad<br>
+                        <span>&#8226;</span>Infecciones de Transmisión Sexual<br>
+                        <span>&#8226;</span>Embarazo<br>
+                        <span>&#8226;</span>Anticoncepción para adolescentes<br>
+                        <span>&#8226;</span>Sexo, Género, Identidad de Género<br>
+                        <span>&#8226;</span>Manejo de afecto y relaciones saludables en adolescentes<br>
+                        <span>&#8226;</span>Asertividad y comunicación verbal entre adolescentes, padres/madres/tutores<br>
+                        <span>&#8226;</span>favorecer autoestima en adolescentes<br>
+                        <span>&#8226;</span>Acompañamiento en toma de decisiones y proyecto de vida en adolescencias
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- Start Bloque dos -->
+
+        <!-- Start actividades -->
+        <div class="container">
+            <div id="actividad-listado"></div>
+            <div id="actividad-container"></div>
+            <button id="btn-continuar-actividad" style="display:none;">Continuar</button>
+        </div>
+        <!-- End actividades -->
     </div>
-    <div class="container">
-        <div id="actividad-listado"></div>
-        <div id="actividad-container"></div>
-        <button id="btn-continuar-actividad" style="display:none;">Continuar</button>
-    </div>
+
 </div>
