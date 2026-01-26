253c7f155926070ae528694549a652b79079ff5a
ledautopica <vane.multimedia@gmail.com>
2026-01-26 16:51:36 -0600

    ajustes enlaces a redes sociales

--- Core/App/View/Layouts/inc/footer.tpl.php
+++ Core/App/View/Layouts/inc/footer.tpl.php
@@ -8,7 +8,6 @@
                 <li><img src="<?php echo URL_PUBLIC ?>recursos/imagenes/stlcc.png" alt=""></li>
                 <li><img src="<?php echo URL_PUBLIC ?>recursos/imagenes/agah.png" alt=""></li>
                 <li><img src="<?php echo URL_PUBLIC ?>recursos/imagenes/paeah.png" alt=""></li>
-
                 <li><img src="<?php echo URL_PUBLIC ?>recursos/imagenes/sedesol.png" alt=""></li>
                 <li><img src="<?php echo URL_PUBLIC ?>recursos/imagenes/cultivando.png" alt=""></li>
                 <li><img src="<?php echo URL_PUBLIC ?>recursos/imagenes/ue.png" alt=""></li>
@@ -20,22 +19,24 @@
     <div class="info-footer">
         <div class="uno-info-footer">
             <p> ESI Honduras - 2025</p>
-            <?php if(!isset($_SESSION['id'])): ?>
-            <div class="btn-login"> <a href="<?php echo URL_BASE ?>login">Login <i
-                        class="fa-solid fa-sign-in-alt"></i></a></div>
+            <?php if (!isset($_SESSION['id'])): ?>
+                <div class="btn-login"> <a href="<?php echo URL_BASE ?>login">Login <i
+                            class="fa-solid fa-sign-in-alt"></i></a></div>
             <?php else: ?>
-            <div class="btn-login"> <a href="<?php echo URL_BASE ?>dashboard">Dashboard <i
-                        class="fa-solid fa-tachometer-alt"></i></a></div>
-            <div class="btn-login"> <a href="<?php echo URL_BASE ?>logout">Logout <i
-                        class="fa-solid fa-sign-out-alt"></i></a></div>
+                <div class="btn-login"> <a href="<?php echo URL_BASE ?>dashboard">Dashboard <i
+                            class="fa-solid fa-tachometer-alt"></i></a></div>
+                <div class="btn-login"> <a href="<?php echo URL_BASE ?>logout">Logout <i
+                            class="fa-solid fa-sign-out-alt"></i></a></div>
             <?php endif ?>
         </div>
         <div class="dos-info-footer">
             <p>Nuestras redes sociales</p>
             <div class="redes-footer">
-                <a href=""> <i class="fa-brands fa-tiktok"></i></a>
-                <a href=""><i class="fa-brands fa-youtube"></i></a>
-                <a href=""> <i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.instagram.com/saludgobhn" target="_blank"> <i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.facebook.com/saludgobhn" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://x.com/saludgobhn" target="_blank"> <i class="fa-brands fa-x-twitter"></i></a>
+                <a href="https://www.salud.gob.hn/sshome/" target="_blank"> <i class="fa-solid fa-globe"></i></a>
+
             </div>
         </div>
         <div class="tres-info-footer">
@@ -46,9 +47,9 @@
                         <li><a href="index">Inicio</a></li>
                         <li style="flex-direction:column"><a href="quienessomos">Quienes Somos</a>
                             <ul class="sub-menu-footer">
-                                <li><a href="">Educadores</a></li>
-                                <li><a href="">Madres y Padres</a></li>
-                                <li><a href="">Jóvenes</a></li>
+                                <li><a href="educadores">Educadores</a></li>
+                                <li><a href="madresypadres">Madres y Padres</a></li>
+                                <li><a href="jovenes">Jóvenes</a></li>
                             </ul>
                         </li>
                         <li><a href="saludsexual">Salud Sexual</a></li>
--- Core/App/View/Layouts/inc/head.tpl.php
+++ Core/App/View/Layouts/inc/head.tpl.php
@@ -1,12 +1,13 @@
 <?php
 ?>
-<div id="head" class="back-head" >
-    <span id="toggle-menu"><i class="fa-solid fa-bars"></i></span> 
+<div id="head" class="back-head">
+    <span id="toggle-menu"><i class="fa-solid fa-bars"></i></span>
     <div class="elementos-head">
         <p>Nuestras redes sociales</p>
-        <a href="http://www.tiktok.com"> <i class="fa-brands fa-tiktok"></i></a>
-        <a href="http://www.youtube.com"><i class="fa-brands fa-youtube"></i></a>
-        <a href="http://www.instagram.com"> <i class="fa-brands fa-instagram"></i></a>
+        <a href="https://www.instagram.com/saludgobhn" target="_blank"> <i class="fa-brands fa-instagram"></i></a>
+        <a href="https://www.facebook.com/saludgobhn" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+        <a href="https://x.com/saludgobhn" target="_blank"> <i class="fa-brands fa-x-twitter"></i></a>
+        <a href="https://www.salud.gob.hn/sshome/" target="_blank"> <i class="fa-solid fa-globe"></i></a>
     </div>
 
 </div>
