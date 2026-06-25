cab08ab9164b869824731c2dfaab3c2c7a69e2ad
miqui-tafoya <taro.miqui@gmail.com>
2026-06-24 22:20:47 -0600

    tracker de matomo;cambio de escudos gov

--- Core/App/View/Layouts/inc/meta.tpl.php
+++ Core/App/View/Layouts/inc/meta.tpl.php
@@ -18,12 +18,29 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400..800&family=Onest:wght@100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400..800&family=Onest:wght@100..900&display=swap"
+    rel="stylesheet">
 <?php
 foreach ($metaParams[1] as $key => $value) {
     echo '<link rel="stylesheet" type="text/css" href="' . URL_PUBLIC . 'styles/' . $value . '.css">' . PHP_EOL;
 }
 ?>
+<!-- Matomo -->
+<script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function () {
+        var u = "//localhost/prod/esihonduras/Public/analytics/";
+        _paq.push(['setTrackerUrl', u + 'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+    })();
+</script>
+<!-- End Matomo Code -->
+
 <title>
     <?php echo SITE_NAME; ?> -
     <?php echo $metaParams[0]; ?>
--- Core/App/View/Layouts/inc/metaerror.tpl.php
+++ Core/App/View/Layouts/inc/metaerror.tpl.php
@@ -28,4 +28,20 @@ foreach ($metaParams[1] as $key => $value) {
     echo '<link rel="stylesheet" type="text/css" href="' . URL_PUBLIC . 'styles/' . $value . '.css">' . PHP_EOL;
 }
 ?>
+<!-- Matomo -->
+<script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function () {
+        var u = "//localhost/prod/esihonduras/Public/analytics/";
+        _paq.push(['setTrackerUrl', u + 'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+    })();
+</script>
+<!-- End Matomo Code -->
+
 <title><?php echo SITE_NAME; ?> - Error <?php echo $error ?></title>
